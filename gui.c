71beddc53b2e73d54ead9f735e74300a3ded8df2
Reidruss <reidrussell217@gmail.com>
2025-01-15 10:49:47 -0600

    Added Creation and Deletion features

--- gui.c
+++ gui.c
@@ -9,7 +9,7 @@ GtkWidget *create_main_window(GtkApplication *app) {
     GtkWidget *file_view;
     GtkWidget *scroll_window;
 
-    // Window setup
+    /* Setting up the window. */
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "FileFind");
     gtk_window_set_default_size(GTK_WINDOW(window), 1400, 900);
@@ -17,32 +17,45 @@ GtkWidget *create_main_window(GtkApplication *app) {
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_window_set_child(GTK_WINDOW(window), box);
 
-    // Search bar
+    /* Setting up Search Bar for directories. */
     search_bar = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(search_bar), "Enter directory path...");
     gtk_box_append(GTK_BOX(box), search_bar);
 
-    // List view setup
+    /* Setting up list for viewing files in a directory. */
     GtkStringList *file_list = gtk_string_list_new(NULL);
-    GtkSingleSelection *selection = gtk_single_selection_new(G_LIST_MODEL(file_list));
+    GtkSingleSelection *single_selection = gtk_single_selection_new(G_LIST_MODEL(file_list));
     GtkListItemFactory *factory = gtk_signal_list_item_factory_new();
 
     g_signal_connect(factory, "setup", G_CALLBACK(setup_list_item), NULL);
     g_signal_connect(factory, "bind", G_CALLBACK(bind_list_item), NULL);
 
-    file_view = gtk_list_view_new(GTK_SELECTION_MODEL(selection), factory);
+    file_view = gtk_list_view_new(GTK_SELECTION_MODEL(single_selection), factory);
 
-    // Scrollable window
+    /* Adding scroll function. */
     scroll_window = gtk_scrolled_window_new();
     gtk_widget_set_vexpand(scroll_window, TRUE);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scroll_window), file_view);
+    gtk_scrolled_window_set_has_frame(GTK_SCROLLED_WINDOW(scroll_window), TRUE);
+
+    /* Add Create and Delete buttons */
+    GtkWidget *create_button = gtk_button_new_with_label("Create File");
+    GtkWidget *delete_button = gtk_button_new_with_label("Delete File");
+
+    gtk_box_append(GTK_BOX(box), create_button);
+    gtk_box_append(GTK_BOX(box), delete_button);
+
+    // Connect signals to their respective callbacks
+    g_signal_connect(create_button, "clicked", G_CALLBACK(on_create_file), file_list);
+    g_signal_connect(delete_button, "clicked", G_CALLBACK(on_delete_file), single_selection);
 
     gtk_box_append(GTK_BOX(box), scroll_window);
 
-    // Connect search bar signal
+    /* Search bar signal. */ 
     g_signal_connect(search_bar, "activate", G_CALLBACK(on_directory_search), file_list);
 
     return window;
 }
+
 
 
--- ops.c
+++ ops.c
@@ -10,15 +10,16 @@ void on_directory_search(GtkEntry *entry, gpointer user_data) {
 
     static unsigned int i = 0;
     unsigned int j;
-    
+
+    /* Erasing the list to add new files. */
     for (int j = 0; j < i ; j++) {
         gtk_string_list_remove(files, 0);
     }
-
-    // Open directory and append file names
+    
     DIR *dir = opendir(path);
     if (!dir) {
         g_print("Invalid Path: %s\n", path);
+        i = 0;
         return;
     }
 
@@ -43,4 +44,78 @@ void bind_list_item(GtkListItemFactory *factory, GtkListItem *list_item, gpointe
     GtkWidget *label = gtk_list_item_get_child(list_item);
     GtkStringObject *string_obj = gtk_list_item_get_item(list_item);
     gtk_label_set_text(GTK_LABEL(label), gtk_string_object_get_string(string_obj));
+}
+
+void on_create_file(GtkWidget *button, gpointer user_data) {
+    GtkWidget *dialog, *entry, *content_area;
+    GtkStringList *files = GTK_STRING_LIST(user_data);
+
+    // Create the dialog
+    dialog = gtk_dialog_new_with_buttons(
+        "Create File",
+        NULL,
+        GTK_DIALOG_MODAL,
+        "_Cancel", GTK_RESPONSE_CANCEL,
+        "_Create", GTK_RESPONSE_ACCEPT,
+        NULL);
+
+    // Add an entry to the content area
+    content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Enter file name...");
+    gtk_box_append(GTK_BOX(content_area), entry);
+    gtk_widget_show(entry);
+
+    // Connect the response signal
+    g_signal_connect(dialog, "response", G_CALLBACK(on_create_file_response), files);
+
+    gtk_widget_show(dialog);
+}
+
+void on_create_file_response(GtkDialog *dialog, int response_id, gpointer user_data) {
+    GtkWidget *entry = gtk_widget_get_first_child(gtk_dialog_get_content_area(dialog));
+    GtkStringList *files = GTK_STRING_LIST(user_data);
+
+    if (response_id == GTK_RESPONSE_ACCEPT) {
+        GtkEntryBuffer *buffer = gtk_entry_get_buffer(GTK_ENTRY(entry));
+        const gchar *filename = gtk_entry_buffer_get_text(buffer);
+
+        if (strlen(filename) > 0) {
+            FILE *file = fopen(filename, "w");
+            if (file) {
+                fclose(file);
+                gtk_string_list_append(files, filename);
+            } else {
+                g_print("Failed to create file: %s\n", filename);
+            }
+        } else {
+            g_print("No file name provided.\n");
+        }
+    }
+
+    gtk_window_destroy(GTK_WINDOW(dialog));
+}
+
+
+void on_delete_file(GtkWidget *button, gpointer user_data) {
+    GtkSingleSelection *single_selection = GTK_SINGLE_SELECTION(user_data);
+
+    /* Finding the postion of the selected file. */
+    guint position = gtk_single_selection_get_selected(single_selection);
+    if (position == GTK_INVALID_LIST_POSITION) {
+        g_print("No file selected for deletion.\n");
+        return;
+    }
+
+    GtkStringObject *string_obj = GTK_STRING_OBJECT(gtk_single_selection_get_selected_item(single_selection));
+    const gchar *file_name = gtk_string_object_get_string(string_obj);
+
+    /* Deleting the file if able to and removing from the StringList. */
+    if (remove(file_name) == 0) {
+        g_print("File deleted: %s\n", file_name);
+        gtk_string_list_remove(GTK_STRING_LIST(gtk_single_selection_get_model(single_selection)), position);
+    } else {
+        g_print("Failed to delete file: %s\n", file_name);
+    }
+    
 }
